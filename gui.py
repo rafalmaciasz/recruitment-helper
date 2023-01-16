613ea8d51dd3f9766bc4e33e2cf6e336fa363078
pacio <mipajor@sstudent.agh.edu.pl>
2023-01-16 16:35:41 +0100

    test

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import pandas as pd
+import csv
 _VARS = {'window': False,
          'fig_agg': False,
          'pltFig': False}
@@ -8,7 +9,7 @@ list_m=['TOPSIS','RSM',"SAFETY PRINCIPAL","UTA"]
 list_k =['Miasto', 'Nazwa kierunku', 'Nazwa uczelni',
        'Procent zdawalności (0-100)', 'Ocena absolwentów (1-5)',
        'Własna ocena sylabusa (1-5)', 'Ilość semestrów',
-       'Próg rekrutacji w poprzednim roku (0-100)', 'KrytX', 'KrytY',
+       'Próg rekrutacji w poprzednim roku (0-100)',
        'Rodzaj kierunku (tech. hum. ekon. itd)']
 list_kryt=["Kry "+str(i) for i in range(len(list_k))]
 td=[]
@@ -37,13 +38,16 @@ while True:
     if event == sg.WIN_CLOSED :
         break
     if event == "-BUTTON_INSERT-":
-        df = pd.read_excel(r"SWD_DB.xlsx")
-        if df is not None:
+        with open("SWD_DB.csv", 'r') as file:
+            data = csv.reader(file)
+            list_of_csv = list(map(tuple, data))
+        print(len(list_of_csv))
+        if len(list_of_csv) != 0:
             sg.popup('Pobrano dane',line_width=40)
         else:
             sg.popup('Brak danych')
     if event == "-BUTTON_RANKING-":
-        for i in range(df.shape[0]):
-            td.append(df.iloc[i].values)
+        for i in range(1,len(list_of_csv)):
+            td.append(list_of_csv[i])
         _VARS['window']['-TABLE_KRYT-'].update(values=td)
 _VARS['window'].close()
